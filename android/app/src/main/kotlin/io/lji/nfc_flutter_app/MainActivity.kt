288cdc20af8dd7a2e4c2c2d2e75e33b3ccb71d48
Ankit Dubey <ankit.dubey@engineerbabu.in>
2020-11-24 13:15:58 +0530

    BL-26836 Initial commit

---
+++ android/app/src/main/kotlin/io/lji/nfc_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.lji.nfc_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/read_example.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:nfc_in_flutter/nfc_in_flutter.dart';
+
+class ReadExampleScreen extends StatefulWidget {
+  @override
+  _ReadExampleScreenState createState() => _ReadExampleScreenState();
+}
+
+class _ReadExampleScreenState extends State<ReadExampleScreen> {
+  StreamSubscription<NDEFMessage> _stream;
+  String btnMessage="Start Scanning";
+  String payloadData="";
+  void _startScanning() {
+    setState(() {
+      btnMessage = "Stop Scanning";
+      _stream = NFC
+          .readNDEF(alertMessage: "Custom message with readNDEF#alertMessage")
+          .listen((NDEFMessage message) {
+        if (message.isEmpty) {
+          print("Read empty NDEF message");
+          return;
+        }
+        print("Read NDEF message with ${message.records.length} records");
+
+        if(message.records.length>0){
+          NDEFRecord record = message.records[0];
+          setState(() {
+            payloadData=record.payload;
+          });
+        }
+
+        /*for (NDEFRecord record in message.records) {
+          payloadData=record.payload;
+          print(
+              "Uff! '${record.id ?? "[NO ID]"}' with TNF '${record.tnf}', type '${record.type}', payload '${record.payload}' and data '${record.data}' and language code '${record.languageCode}'");
+        }*/
+      }, onError: (error) {
+        setState(() {
+          _stream = null;
+        });
+        if (error is NFCUserCanceledSessionException) {
+          print("user canceled");
+        } else if (error is NFCSessionTimeoutException) {
+          print("session timed out");
+        } else {
+          print("error: $error");
+        }
+      }, onDone: () {
+        setState(() {
+          _stream = null;
+        });
+      });
+    });
+  }
+
+  void _stopScanning() {
+    _stream?.cancel();
+    setState(() {
+      btnMessage = "Start Scanning";
+      _stream = null;
+    });
+  }
+
+  void _toggleScan() {
+    if (_stream == null) {
+      _startScanning();
+    } else {
+      _stopScanning();
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stopScanning();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Read NFC example"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 32,),
+            Text(payloadData,style: TextStyle(fontSize: 20),),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 64,vertical: 32),
+              child: RaisedButton(
+                child: Text(btnMessage),
+                onPressed: _toggleScan,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
